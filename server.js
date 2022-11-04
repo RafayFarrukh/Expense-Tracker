07465e7436c9c99695496fda208b254c3fb345b9
unknown <rafayfarrukh941@gmail.com>
2022-11-04 14:45:14 +0500

    Deploy to heroku

--- server.js
+++ server.js
@@ -16,18 +16,26 @@ var bodyParser = require("body-parser");
 const cors = require("cors");
 app.use(cors());
 app.use(express.static(path.join(__dirname, "client/build")));
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./client/build/index.html")); // <- Here !
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "./client/build/index.html")); // <- Here !
+// });
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build")); // serve the static react app
+  app.get(/^\/(?!api).*/, (req, res) => {
+    // don't serve api routes to react app
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  });
+  console.log("Serving React App...");
+}
 app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/users", userRoutes);
 app.use("/expenses", apiauth, expenseRoutes);
 app.use("/", apiauth, homeRoutes);
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+// }
 
 // if (process.env.NODE_ENV === "production") {
 //   app.use(express.static(path.join(__dirname, "client", "build")));
