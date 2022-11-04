eb91fe56e65fa703a2884956fea59969b89f8577
unknown <rafayfarrukh941@gmail.com>
2022-11-04 14:50:57 +0500

    Deploy to heroku

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build")); // serve the static react app
   app.get(/^\/(?!api).*/, (req, res) => {
     // don't serve api routes to react app
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+    res.sendFile(path.join(__dirname, "./client/build"));
   });
   console.log("Serving React App...");
 }
